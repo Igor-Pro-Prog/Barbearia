c3b4137480370b5da0a083685ecffaab7b1513c2
subrs100zero <112484364+subrs100zero@users.noreply.github.com>
2023-05-05 18:12:45 -0300

    Update AgendamentoRepositorio.java

--- Barbearia-main/barbearia/repositorio/AgendamentoRepositorio.java
+++ Barbearia-main/barbearia/repositorio/AgendamentoRepositorio.java
@@ -1,7 +1,5 @@
 package repositorio;
-import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import dados.Agendamento;
@@ -13,16 +11,6 @@ public class AgendamentoRepositorio {
 
     public void adicionar(Agendamento agendamento) {
         agendamentos.add(agendamento);
-    }
-
-    public List<Agendamento> buscarPorData(Date data) {
-        List<Agendamento> agendamentosEncontrados = new ArrayList<>();
-        for (Agendamento agendamento : agendamentos) {
-            if (agendamento.getData().equals(data)) {
-                agendamentosEncontrados.add(agendamento);
-            }
-        }
-        return agendamentosEncontrados;
     }
 
     public List<Agendamento> buscarPorFuncionario(Funcionario funcionario) {
@@ -48,7 +36,7 @@ public class AgendamentoRepositorio {
     public List<Agendamento> buscarTodos() {
         return agendamentos;
     }
-
+    
     // outros métodos de busca
 
     public void atualizar(Agendamento agendamento) {
@@ -65,17 +53,6 @@ public class AgendamentoRepositorio {
     }
 
     public void adicionarAgendamento(Agendamento agendamento) {
+    	agendamentos.add(agendamento);
     }
-
-    public List<Agendamento> buscarAgendamentosPorData(List<Agendamento> agendamentos, LocalDate data) {
-        List<Agendamento> agendamentosNaData = new ArrayList<>();
-        for (Agendamento agendamento : agendamentos) {
-            if (agendamento.getData().equals(data)) {
-                agendamentosNaData.add(agendamento);
-            }
-        }
-        return agendamentosNaData;
-    }
-    
-    
 }
