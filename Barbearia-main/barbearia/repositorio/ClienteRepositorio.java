bb23466301ef5acdf25d75a01c83fd76ae13c8fc
subrs100zero <112484364+subrs100zero@users.noreply.github.com>
2023-05-05 18:13:07 -0300

    Update ClienteRepositorio.java

--- Barbearia-main/barbearia/repositorio/ClienteRepositorio.java
+++ Barbearia-main/barbearia/repositorio/ClienteRepositorio.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 import java.util.List;
 import dados.Cliente;
 
-
 public class ClienteRepositorio {
     private List<Cliente> clientes;
 
@@ -15,51 +14,19 @@ public class ClienteRepositorio {
         clientes.add(cliente);
     }
 
-    public boolean removerCliente(int id) {
-        for (Cliente cliente : clientes) {
-            if (cliente.getId() == id) {
-                clientes.remove(cliente);
-                return true;
-            }
-        }
-        return false;
-    }
-
     public List<Cliente> listarClientes() {
         return clientes;
     }
 
-    public Cliente buscarCliente(int id) {
-        for (Cliente cliente : clientes) {
-            if (cliente.getId() == id) {
-                return cliente;
-            }
-        }
-        return null;
-    }
-
-    public Cliente[] buscarTodos() {
-        return clientes.toArray(new Cliente[0]);
+    public List<Cliente> buscarTodos() {
+        return clientes;
     }
 
     public boolean existe(Cliente cliente) {
         return clientes.contains(cliente);
     }
 
-    public boolean existeNome(int id) {
-        for (Cliente cliente : clientes) {
-            if (cliente.getId() == id) {
-                return true;
-            }
-        }
-        return false;
-    }
-
     public Cliente buscarClientePorNome(String nomeCliente) {
         return clientes.stream().filter(cliente -> cliente.getNome().equals(nomeCliente)).findFirst().orElse(null);
-    }
-
-    public Cliente existeNome(String nomeCliente) {
-        return null;
     }
 }
